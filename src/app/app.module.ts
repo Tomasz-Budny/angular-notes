b3f0d1c86da8c1b0e9bbc7381a011669b810fff9
Tomasz-Budny <s208246@sggw.edu.pl>
2022-12-19 21:08:04 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { TutorialSectionComponent } from './tutorial-section/tutorial-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivesComponent,
+    TutorialSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tutorial-section/tutorial-section.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-section',
+  templateUrl: './tutorial-section.component.html',
+  styleUrls: ['./tutorial-section.component.css']
+})
+export class TutorialSectionComponent {
+  isVisible: boolean = true;
+  @Input() nameOfSection: string = "";
+}
